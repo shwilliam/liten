f326053b123fd5f5ef06ff7c3bd2578bcaa230f5
William Lindvall <shwilliam@hey.com>
2020-07-10 12:45:21 -0700

    remove ig-related elements from client

--- client/src/App.js
+++ client/src/App.js
@@ -11,23 +11,20 @@ export const App = () => {
     setError()
     setLoading(true)
 
-    const {
-      input_url,
-      input_text,
-      input_ig,
-      input_tweet,
-    } = e.currentTarget.elements
+    const {input_url, input_text, input_tweet} = e.currentTarget.elements
 
     try {
+      const body = JSON.stringify({
+        url: input_url.value,
+        text: input_text.value,
+        tweet: input_tweet.value,
+      })
+
       const response = await fetch(process.env.REACT_APP_API_URL, {
         method: 'POST',
-        body: JSON.stringify({
-          url: input_url.value,
-          text: input_text.value,
-          ig: input_ig.value,
-          tweet: input_tweet.value,
-        }),
+        body,
       })
+
       const responseJson = await response.json()
 
       setSocials(responseJson)
@@ -56,14 +53,6 @@ export const App = () => {
           <label htmlFor="input_text">URL text:</label>
           <input name="input_text" id="input_text" type="text" />
 
-          <label htmlFor="input_ig">Instagram handle:</label>
-          <input
-            name="input_ig"
-            id="input_ig"
-            type="text"
-            placeholder="coolkid97"
-          />
-
           <label htmlFor="input_tweet">Tweet URL:</label>
           <input
             name="input_tweet"
@@ -86,13 +75,13 @@ export const App = () => {
         {error && <p className="error">{error}</p>}
 
         {socials && (
-          <p className="socials">
+          <div className="socials">
             {(socials.url || socials.text?.length > 0) && (
               <section>
                 <table>
                   <thead>
                     <tr>
-                      <th colspan="5">
+                      <th colSpan="5">
                         <h3>Site metadata</h3>
                       </th>
                     </tr>
@@ -107,27 +96,24 @@ export const App = () => {
                   <tbody>
                     {socials.url && (
                       <tr>
-                        {Object.values(socials.url).map(val => (
-                          <td>{val}</td>
-                        ))}
+                        <td>{socials.url.url}</td>
+                        <td>{socials.url.title}</td>
+                        <td>{socials.url.favicon}</td>
+                        <td>{socials.url.description}</td>
+                        <td>{socials.url.image}</td>
                       </tr>
                     )}
                     {socials.text?.map(socials => (
-                      <tr>
-                        {Object.values(socials).map(val => (
-                          <td>{val}</td>
-                        ))}
+                      <tr key={`socials-text-${socials.url}`}>
+                        <td>{socials.url}</td>
+                        <td>{socials.title}</td>
+                        <td>{socials.favicon}</td>
+                        <td>{socials.description}</td>
+                        <td>{socials.image}</td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
-              </section>
-            )}
-
-            {socials.ig && (
-              <section>
-                <h3>Instagram</h3>
-                {socials.ig && <img src={socials.ig} alt="Instagram post" />}
               </section>
             )}
 
@@ -141,7 +127,7 @@ export const App = () => {
                 )}
               </section>
             )}
-          </p>
+          </div>
         )}
       </section>
     </>

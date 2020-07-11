e6c7cfea739e2bc28fbbc35a9c330647344b849d
William Lindvall <shwilliam@hey.com>
2020-07-10 18:21:52 -0700

    style

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,42 @@
 import React, {useState} from 'react'
 
+const SocialTargetsSelectButton = ({value, onClick, children}) => {
+  const handleClick = () => onClick(value)
+
+  return (
+    <button
+      className="social-targets-select__button"
+      type="button"
+      value={value}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  )
+}
+const SocialTargetsSelect = ({onChange}) => (
+  <ul className="social-targets-select align-center">
+    <li className="social-targets-select__item">
+      <SocialTargetsSelectButton value="tweet" onClick={onChange}>
+        generate tweet previews
+      </SocialTargetsSelectButton>
+    </li>
+    <li className="social-targets-select__item">
+      <SocialTargetsSelectButton value="url" onClick={onChange}>
+        view site metatags
+      </SocialTargetsSelectButton>
+    </li>
+    <li className="social-targets-select__item">
+      <SocialTargetsSelectButton value={['tweet', 'url']} onClick={onChange}>
+        show it all
+      </SocialTargetsSelectButton>
+    </li>
+  </ul>
+)
+
 export const App = () => {
+  const [socialTargets, setSocialTargets] = useState()
+
   const [error, setError] = useState()
   const [socials, setSocials] = useState()
   const [loading, setLoading] = useState(false)
@@ -11,12 +47,14 @@ export const App = () => {
     setError()
     setLoading(true)
 
-    const {input_url, input_tweet} = e.currentTarget.elements
+    const formElements = e.currentTarget.elements
+    const input_tweet = formElements[0] // FIXME
+    const input_url = formElements?.input_url
 
     try {
       const body = JSON.stringify({
-        url: input_url.value,
-        tweet: input_tweet.value,
+        tweet: input_tweet?.value,
+        url: input_url?.value,
       })
 
       const response = await fetch(process.env.REACT_APP_API_URL, {
@@ -37,86 +75,119 @@ export const App = () => {
 
   return (
     <>
-      <section>
-        <h2 className="hide-screens">Get socials form</h2>
+      {!socialTargets && (
+        <section className="social-targets-select">
+          <SocialTargetsSelect onChange={setSocialTargets} />
+        </section>
+      )}
 
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="input_url">URL(s):</label>
-          <textarea
-            name="input_url"
-            id="input_url"
-            rows="3"
-            placeholder="https://github.com/shwilliam"
-          ></textarea>
+      {socialTargets && !socials && (
+        <section>
+          <h2 className="hide-screens">Get socials form</h2>
 
-          <label htmlFor="input_tweet">Tweet URL:</label>
-          <input
-            name="input_tweet"
-            id="input_tweet"
-            type="url"
-            placeholder="https://twitter.com/shwilliam/status/1337"
-          />
-
-          <button type="submit" disabled={loading}>
-            GET SOCIALS
-          </button>
-        </form>
-      </section>
-
-      <section aria-live="polite">
-        <h2 className="hide-screens">Socials results</h2>
-
-        {loading && <p className="loading">loading...</p>}
-
-        {error && <p className="error">{error}</p>}
-
-        {socials && (
-          <div className="socials">
-            {socials.url?.length > 0 && (
-              <section>
-                <table>
-                  <thead>
-                    <tr>
-                      <th colSpan="5">
-                        <h3>Site metadata</h3>
-                      </th>
-                    </tr>
-                    <tr>
-                      <th>URL</th>
-                      <th>Title</th>
-                      <th>Favicon</th>
-                      <th>Description</th>
-                      <th>Image</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {socials.url?.map(site => (
-                      <tr key={`socials-url-${site.url}`}>
-                        <td>{site.url}</td>
-                        <td>{site.title}</td>
-                        <td>{site.favicon}</td>
-                        <td>{site.description}</td>
-                        <td>{site.image}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </section>
+          <form onSubmit={handleSubmit}>
+            {socialTargets.includes('tweet') && (
+              <>
+                <label htmlFor="input_tweet">Tweet URL</label>
+                <input
+                  name="input_tweet"
+                  id="input_tweet"
+                  type="url"
+                  placeholder="https://twitter.com/shwilliam/status/1337"
+                  required={socialTargets.length === 1}
+                />
+              </>
             )}
 
-            {socials.tweet && (
-              <section>
-                <h3>Twitter</h3>
-                {socials.tweet?.embed && <code>{socials.tweet?.embed}</code>}
+            {socialTargets.includes('url') && (
+              <>
+                <label htmlFor="input_url">URL(s)</label>
+                <textarea
+                  name="input_url"
+                  id="input_url"
+                  rows="3"
+                  placeholder="https://github.com/shwilliam"
+                  required={socialTargets.length === 1}
+                ></textarea>
+              </>
+            )}
 
-                {socials.tweet?.preview && (
-                  <img src={socials.tweet?.preview} alt="Twitter preview" />
+            <button type="submit" disabled={loading}>
+              get socials
+            </button>
+          </form>
+        </section>
+      )}
+
+      {socialTargets && socials && (
+        <>
+          {Object.keys(socials)?.length === 0 && (
+            <p>
+              Nothing found{' '}
+              <span role="img" aria-label="">
+                🥺
+              </span>
+            </p>
+          )}
+
+          <section aria-live="polite">
+            <h2 className="hide-screens">Socials results</h2>
+
+            {loading && <p className="loading">loading...</p>}
+
+            {error && <p className="error">{error}</p>}
+
+            {socials && (
+              <div className="socials">
+                {socials.url?.length > 0 && (
+                  <section>
+                    <table>
+                      <thead>
+                        <tr>
+                          <th colSpan="5">
+                            <h3 className="subtitle">Site metadata</h3>
+                          </th>
+                        </tr>
+                        <tr>
+                          <th>URL</th>
+                          <th>Title</th>
+                          <th>Favicon</th>
+                          <th>Description</th>
+                          <th>Image</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {socials.url?.map(site => (
+                          <tr key={`socials-url-${site.url}`}>
+                            <td>{site.url}</td>
+                            <td>{site.title}</td>
+                            <td>{site.favicon}</td>
+                            <td>{site.description}</td>
+                            <td>{site.image}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </section>
                 )}
-              </section>
+
+                {socials.tweet && (
+                  <section>
+                    <h3 className="subtitle">Twitter</h3>
+                    {socials.tweet?.embed && (
+                      <code>{socials.tweet?.embed}</code>
+                    )}
+
+                    {socials.tweet?.preview && (
+                      <img src={socials.tweet?.preview} alt="Twitter preview" />
+                    )}
+                  </section>
+                )}
+              </div>
             )}
-          </div>
-        )}
-      </section>
+          </section>
+        </>
+      )}
     </>
   )
 }

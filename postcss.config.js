387f8852e93cb64a5acc7117f9f871c41b41f4ed
William Lindvall <shwilliam@hey.com>
2020-07-28 00:26:11 -0700

    fix `purgecss` ignoring nested files

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
     ...(process.env.NODE_ENV === 'production'
       ? {
           '@fullhuman/postcss-purgecss': {
-            content: ['./components/*.tsx', './pages/*.tsx'],
+            content: ['./components/**/*.tsx', './pages/**/*.tsx'],
             defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
           },
         }

775b47cb978a939574865f477da54979d1c5dc9f
William Lindvall <shwilliam@hey.com>
2020-08-18 15:46:30 -0700

    update inverted `LinkButton` hover styles

--- components/link-button.tsx
+++ components/link-button.tsx
@@ -17,7 +17,7 @@ const LinkButton = ({
   <a
     className={`inline-block px-3 py-2 font-bold text-white border-4 border-blue-500 ${
       invert
-        ? 'bg-white text-blue-500 hover:bg-blue-500 hover:text-white'
+        ? 'bg-white text-blue-500 hover:opacity-50'
         : 'bg-blue-500 hover:bg-white hover:text-blue-500'
     } ${className}`}
     href={href}

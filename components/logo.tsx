41753fe05fa1dcf6a71007311f79e4907fe6ccbb
William Lindvall <shwilliam@hey.com>
2020-08-18 18:17:56 -0700

    fill logo icon

--- components/logo.tsx
+++ components/logo.tsx
@@ -7,7 +7,7 @@ const Logo = () => (
   >
     <path
       d="M36 34v1a8 8 0 0 0 8 8h10a8 8 0 0 0 8-8v-6a8 8 0 0 0-8-8H44a8 8 0 0 0-8 8v1m4 0v-1a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4v6a4 4 0 0 1-4 4H44a4 4 0 0 1-4-4v-1m-12-4v-1a8 8 0 0 0-8-8H10a8 8 0 0 0-8 8v6a8 8 0 0 0 8 8h10a8 8 0 0 0 8-8v-1m-4 0v1a4 4 0 0 1-4 4H10a4 4 0 0 1-4-4v-6a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4v1"
-      fill="none"
+      fill="#fff"
       stroke="currentColor"
       strokeMiterlimit="10"
       strokeWidth="2"
@@ -16,7 +16,7 @@ const Logo = () => (
     ></path>
     <path
       d="M48.3 32a2 2 0 0 1-2 2h-29a2 2 0 0 1-2-2 2 2 0 0 1 2-2h29a2 2 0 0 1 2 2z"
-      fill="none"
+      fill="#fff"
       stroke="currentColor"
       strokeMiterlimit="10"
       strokeWidth="2"
